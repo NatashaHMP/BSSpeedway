a7b631c124b6ac3ba2a06596c3c4382f38249363
gustavox13 <gurodrigues1300@gmail.com>
2017-12-27 11:14:25 -0200

    add death screen and win screen

---
+++ Assets/Scripts/HUD/WinCondition.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+
+    [SerializeField]
+    private Canvas WinScreen;
+    private GameObject objPlayer;
+    [SerializeField]
+    private Win WinScript;
+    private string objectTag;
+
+ 
+
+    private void Start()
+    {
+        LoadResources();
+    }
+
+    private void LoadResources()
+    {
+        Debug.Log(Time.timeScale);
+        //objectTag = Constants.TagName.PLAYER;
+        objectTag = "Player";
+        WinScreen.enabled = false;
+        objPlayer = GameObject.FindWithTag(objectTag);   
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == objectTag)
+        {
+            WinGame();
+        }
+    }
+
+    private void WinGame()
+    {
+        WinScript.UnlockNextLevel();
+        WinScreen.enabled = true;
+        Time.timeScale = 0.0f;
+    }
+
+
+}
---
+++ Assets/Scripts/Player/DeathPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathPlayer : MonoBehaviour
+{
+
+
+    private string objectTag;
+
+    public bool PlayerDied
+    {
+        get { return playerDied; }
+        set { playerDied = value; }
+    }
+    private bool playerDied;
+
+    private void Start()
+    {
+        LoadResources();
+    }
+
+
+    private void LoadResources()
+    {
+        playerDied = false;
+        objectTag = "Floor";
+        //objectTag = Constants.TagName.FLOOR;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == objectTag)
+        {
+            playerDied = true;
+        }
+    }
+}

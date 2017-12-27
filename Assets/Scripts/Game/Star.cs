ffe7c9f5380a7e3477a08001580d99dd75a6d280
gustavox13 <gurodrigues1300@gmail.com>
2017-12-27 14:35:20 -0200

    stars system

---
+++ Assets/Scripts/Game/Star.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    [SerializeField]
+    private StarCount starCountScript;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == /*Constants.TagName.PLAYER*/ "Player")
+        {
+            AddStar();
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void AddStar()
+    {
+        starCountScript.NumberStars++;
+    }
+
+}

0d45e2fd4a02018545172b575b4b5f71df96c452
gustavox13 <gurodrigues1300@gmail.com>
2018-01-02 16:21:49 -0200

    add light effects to lvl 4

---
+++ Assets/Scripts/Game/Orbitar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbitar : MonoBehaviour {
+
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Vector3 eixo;
+    [SerializeField]
+    private float speedRotation;
+    [SerializeField]
+    private float diamiter;
+
+
+
+    private void Update()
+    {
+        OrbitRotation();
+        
+    }
+
+    private void OrbitRotation()
+    {
+        transform.RotateAround(target.position, eixo, speedRotation * Time.deltaTime);
+    }
+}
